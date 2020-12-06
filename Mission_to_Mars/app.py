8e7205a1278ddc036083efd603af8a7275e19454
Kyle O'Malley <kyleomalley@Kyles-MBP-79.lan>
2020-12-05 22:50:54 -0500

    updated multiple files, not working yet

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,1 +1,27 @@
-import flask
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def home():
+
+    mars_dict = mongo.db.mars_dict.find_one()
+    return render_template('index.html', mars=mars_dict)
+
+@app.route("/scrape")
+def scrape():
+
+    mars_dict = mongo.db.mars_dict
+    mars_data = scrape_mars.scrape()
+
+    mars_dict.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,38 +1,38 @@
-from bs4 import BeautifulSoup as bs
 from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
-import os
-import time
 import requests
+import pymongo
 import warnings
 warnings.filterwarnings('ignore')
 
 def init_browser():
     # Setup splinter
-    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+    executable_path = {"executable_path": "/Users/kyleomalley/Downloads/chromedriver"}
     return Browser('chrome', **executable_path, headless=False)
-
-
+    
 
 # scrape function
 def scrape():
     browser = init_browser()
-    mars_info = {}
 
     #Mars News scraped
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
     html = browser.html
-    soup = bs(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
-    title = soup.find_all('div', class_='content_title')[1].text
+    news_title = soup.find_all('div', class_='content_title')[1].text
     news_p = soup.find('div', class_='article_teaser_body').text
+
+    print(news_p)
 
     # Mars image scraped
     image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(image_url)
     html = browser.html
-    soup = bs(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
     
     find_image_url = soup.find('article')['style'].replace('background-image: url(','').replace(');', '')[1:-1]
     featured_image_url = image_url + find_image_url
@@ -50,5 +50,42 @@ def scrape():
     # Hemisphere Images scraped
     hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(hemisphere_url)
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
 
+    hemis = soup.find_all('div', class_='item')
+    main_url = 'https://astrogeology.usgs.gov'
+    hemis_image_urls = []
 
+    for img in hemis:
+        title = img.find('h3').text
+
+        # need link to full res image
+        specific_url = img.find('a', class_='itemLink product-item')['href']
+    
+        # visit link for image
+        browser.visit(main_url + specific_url)
+    
+        # Now get html for page to find src to add to url list
+        specific_url_html = browser.html
+    
+        soup = BeautifulSoup(specific_url_html, 'html.parser')
+    
+        imageurl = main_url + soup.find('img', class_='wide-image')['src']
+    
+        # dictionary for title/url for images
+        # hemis_dict = {}
+        # hemis_dict['title'] = title
+        # hemis_dict['image_url'] = imageurl
+        
+        hemis_image_urls.append({"title" : title, "image_url" : imageurl})
+
+# Dictionary for Scraped html
+    mars_dict = {
+        "news_title": news_title,
+        "news_p": news_p,
+        "featured_image_url": featured_image_url,
+        "facts_html_table": facts_html_table,
+        "hemis_image_urls": hemis_image_urls
+    }
+    return mars_dict
